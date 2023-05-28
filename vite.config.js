29eeb2baf96f63b16e5e1b6d05093a7f227dbf55
r3c4 <vinicius.rech.dev@gmail.com>
2023-05-28 08:40:21 -0300

    fix: the path for barrels in tsconfig.

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,9 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    css: {
-        loaderOptions: {
-            scss: {
-                prependData: `@import "./resources/scss/app.scss"`,
-            },
+    loaderOptions: {
+        scss: {
+            prependData: `@import "./resources/scss/app.scss"`,
         },
     },
     plugins: [
