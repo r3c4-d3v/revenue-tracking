26800347b7eae2f3591229bc5b26c5bbf28d3be3
r3c4 <vinicius.rech.dev@gmail.com>
2023-05-31 00:59:41 -0300

    refactor: application logo barrel has been moved to owner folder.

--- resources/js/Components/ApplicationLogo/index.tsx
+++ resources/js/Components/ApplicationLogo/index.tsx
@@ -2,7 +2,7 @@ import {
     ApplicationLogoSvg,
     LogoProps,
     svgShape,
-} from "@/Barrels/ApplicationLogo";
+} from "@/Components/ApplicationLogo/barrel";
 
 const ApplicationLogo = (props: LogoProps): ApplicationLogoSvg => (
     <svg {...props} viewBox="0 0 316 316" xmlns="http://www.w3.org/2000/svg">
